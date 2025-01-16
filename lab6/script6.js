347bd978cef9d3e5d1aab1d9e7e2a3c1a1b9490e
MorinaAnna <143120694+MorinaAnna@users.noreply.github.com>
2025-01-16 23:31:52 +0200

    Update script6.js

--- lab6/script6.js
+++ lab6/script6.js
@@ -34,16 +34,7 @@ function topic() {
                https://github.com/MorinaAnna/lab6
             </a><br><br>
         </p>
-        <p>Посилання на живу сторінку <b>звітів</b> <br><br>
-            <a href="https://brieflyl.github.io/IC-34_Reports_Perekhrest/" target="_blank">
-                https://brieflyl.github.io/IC-34_Reports_Perekhrest/
-            </a><br><br>
-        </p>
-        <p>Посилання на <b>репозиторій звітів</b> <br><br>
-            <a href="https://github.com/brieflyl/IC-34_Reports_Perekhrest" target="_blank">
-                https://github.com/brieflyl/IC-34_Reports_Perekhrest
-            </a><br><br>
-        </p>
+        
     `;
 }
 
